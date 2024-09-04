34c43926160b04f0e0f9d2c3619dd160a9eb8c31
iwenki <139893979+iwenki@users.noreply.github.com>
2024-09-04 18:00:34 -0500

    Updated Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,28 @@
 import axios from "axios";
 
 export default function Form(){
+  function search() {
+    const apiKey = "b1dc8907c11f6a0fba1c6287831a15d6";
+    let apiUrl = `https://api.exchangeratesapi.io/v1/2023-12-24?access_key=${apiKey}&base=GBP&symbols=USD,CAD,EUR`;
+    axios.get(apiUrl).then(handleResponse);
+  }
   return (
     <div className="informationInput">
       <form>
-
-<div class="input-group mb-3">
-  <input type="text" className="form-control" aria-label="Text input with select form"/>
-  <select className="form-select" aria-label="Default select example">
-        <option selected>Select currency</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-      </select>
-</div>
-
-    </form>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Select a base currency</option>
+          <option value="BHD">Bahraini Dinar</option>
+          <option value="OMR">Omani Rial</option>
+          <option value="GTQ">Guatemalan Quetzal </option>
+          <option value="KWD">Kuwaiti Dinar</option>
+          <option value="USD">U.S. Dollar</option>
+          <option value="GBP">British Pound</option>
+          <option value="MXN">Mexico Peso</option>
+          <option value="JPY">Japan Yen</option>
+          <option value="BRL">Brazil Real</option>
+          <option value="GHS">Ghana New Cedi</option>
+        </select>
+      </form>
     </div>
   );
 }
